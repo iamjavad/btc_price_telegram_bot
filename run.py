3554213f76b20c9192c1326bf66692957efbb64e
javad pournosrat <pournosrat.javad@gmail.com>
2021-07-16 15:23:58 +0430

    creat start message

--- run.py
+++ run.py
@@ -16,7 +16,10 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 		level=logging.INFO)
 
 def start(updater, context):
-	context.bot.send_message(chat_id=updater.effective_chat.id, text="")
+	context.bot.send_message(chat_id=updater.effective_chat.id, text="""Hi
+		welcom to BITCOIN PRICE bot 
+		while you type or click on /price
+		i will send you price of BTC at this time.""")
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
